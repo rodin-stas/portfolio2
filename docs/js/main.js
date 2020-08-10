075b66af0bd62c155bf8bb263dcd21fb153bff64
Stas <rodinstas@gmail.com>
2020-08-10 14:07:07 +0300

    add validation and POST form

--- docs/js/main.js
+++ docs/js/main.js
@@ -93,7 +93,65 @@ $(document).ready(function () {
         this.classList.remove("active");
       }
     });
-  }
+  };
+
+
+   //FORM VALIDATE
+   $('#contacts-form').validate({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        theme: {
+            required: true
+        },
+        message: {
+            required: true
+        }
+    },
+    messages: {
+        email: {
+            required: 'Введите email',
+            email: 'отсутсвует символ @'
+        },
+        theme: {
+            required: 'Введите тему сообщения'
+        },
+        message: {
+            required: 'Введите текст сообщения'
+        }
+    },
+    submitHandler: function (form) {
+        ajaxFormSubmit();
+    }
+
+})
+
+ // Функция AJAX запрса на сервер
+
+ function ajaxFormSubmit() {
+
+  let string = $("#contacts-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+  //Формируем ajax запрос
+  $.ajax({
+      type: "POST", // Тип запроса - POST
+      url: "php/mail.php", // Куда отправляем запрос
+      data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+      // Функция если все прошло успешно
+      success: function (html) {
+          $("#contacts-form").slideUp(800);
+          $('#answer').html(html);
+      }
+  });
+
+  // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+  return false;
+}
+
+
 
   
 });
